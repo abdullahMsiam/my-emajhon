58ff4ce9daaf1dcb0a25c1b0f321f4236e947ec0
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-05-10 20:15:26 +0600

    manage local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
@@ -15,7 +16,13 @@ const Shop = () => {
     const handleAddToCart = (product) => {
         const newCart = [...cart, product]
         setCart(newCart);
+        addToDb(product.id);
+        // console.log(product);
     }
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
+    }, [])
     return (
         <div className='shop-container'>
             <div className='products-container'>
