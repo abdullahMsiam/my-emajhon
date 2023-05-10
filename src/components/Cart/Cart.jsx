b6d134375a2269f8c0b304f25bbbb70830a389df
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-05-11 01:42:07 +0600

    cart quantity

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,18 @@ const Cart = ({ cart }) => {
     // console.log(cart[0]);
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price;
-        shipping = shipping + product.shipping;
+        // if another shortcut:
+        // if (product.quantity === 0) {
+        //     product.quantity = 1;
+        // }
+        // shortcut: s
+        // product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity;
+        // console.log(quantity);
     }
     const tax = total * 5 / 100;
     const grandTotal = total + tax + shipping;
@@ -17,7 +26,7 @@ const Cart = ({ cart }) => {
     return (
         <div className='cart'>
             <h3>Order summary</h3>
-            <p>Selected Item: {cart.length}</p>
+            <p>Selected Item: {quantity}</p>
             <p>Total Price: {total.toFixed(2)} </p>
             <p>Total shipping: {shipping} </p>
             <p>Tax: {tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -14,7 +14,21 @@ const Shop = () => {
     }, [])
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product]
+        // basic new cart set: 
+        // const newCart = [...cart, product]
+
+        // advance sys: 
+        let newCart = [];
+        const exists = cart.find(pd => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
         setCart(newCart);
         addToDb(product.id);
         // console.log(product);
@@ -32,7 +46,7 @@ const Shop = () => {
                 addedProduct.quantity = quantity;
                 // step 4: push element to the addedProduct 
                 savedCart.push(addedProduct);
-                console.log('added product', addedProduct);
+                // console.log('added product', addedProduct);
             }
             // step : set in the cart 
             setCart(savedCart);
